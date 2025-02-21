a6fd14554aba1207913aee2ebd10ca4319da3e6b
Swapnil Baburao Diwate <117917508+diwates2232@users.noreply.github.com>
2025-02-22 00:33:36 +0530

    Update regionRoutes.js

--- regionRoutes.js
+++ regionRoutes.js
@@ -1,1 +1,23 @@
+const express = require("express");
+const {
+    getGlobalSummary,
+    getGlobalDetails,
+    getRegionSummary,
+    getRegionDetails,
+    getDeviceUptimeSummary
+} = require("../controllers/regionControllers");
 
+const router = express.Router();
+
+// Global Routes
+router.get("/summary/global", getGlobalSummary);
+router.get("/details/global", getGlobalDetails);
+
+// Region Routes
+router.get("/summary/:regionName", getRegionSummary);
+router.get("/details/:regionName", getRegionDetails);
+
+// Device Uptime/Downtime Summary
+router.get("/uptime-summary", getDeviceUptimeSummary);
+
+module.exports = router;
